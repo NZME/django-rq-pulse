6f866ba411b4aa36b05dff32df9c11dd4b2b5586
Medhat Gayed <medhat.gayed@grabone.co.nz>
2018-02-12 16:10:10 +1300

    Remove redundant code from setup.py and enable usage of bumpversion.

--- setup.py
+++ setup.py
@@ -10,43 +10,12 @@ except ImportError:
     from distutils.core import setup
 
 
-def get_version(*file_paths):
-    """Retrieves the version from django_rq_pulse/__init__.py"""
-    filename = os.path.join(os.path.dirname(__file__), *file_paths)
-    version_file = open(filename).read()
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError('Unable to find version string.')
-
-
-version = get_version("django_rq_pulse", "__init__.py")
-
-
-if sys.argv[-1] == 'publish':
-    try:
-        import wheel
-        print("Wheel version: ", wheel.__version__)
-    except ImportError:
-        print('Wheel library missing. Please run "pip install wheel"')
-        sys.exit()
-    os.system('python setup.py sdist upload')
-    os.system('python setup.py bdist_wheel upload')
-    sys.exit()
-
-if sys.argv[-1] == 'tag':
-    print("Tagging the version on git:")
-    os.system("git tag -a %s -m 'version %s'" % (version, version))
-    os.system("git push --tags")
-    sys.exit()
-
 readme = open('README.rst').read()
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='django-rq-pulse',
-    version=version,
+    version='0.1.1',
     description="""A Django package to check that rq workers are running and notify admins if they are not""",
     long_description=readme + '\n\n' + history,
     author='Medhat Gayed',
